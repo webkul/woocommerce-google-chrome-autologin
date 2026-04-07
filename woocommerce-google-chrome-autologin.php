02d0283f7e2dcb0aa33464b840681463a86994b1
dineshyadav.wp <dineshyadav.wp@webkul.in>
2026-04-07 14:54:51 +0530

    chore: bump plugin version to 1.0.4

--- woocommerce-google-chrome-autologin.php
+++ woocommerce-google-chrome-autologin.php
@@ -3,7 +3,7 @@
 * Plugin Name: WooCommerce Chrome Login
 * Plugin URI: https://webkul.com
 * Description: Wordpress WooCommerce Chrome Login Plugin customers to remember their login credentials and allow auto login.
-* Version: 1.0.3
+* Version: 1.0.4
 * Author: Webkul
 * Text Domain: wc_chrome_auto_login
 **/
